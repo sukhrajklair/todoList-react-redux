cd9560505e94af1426afe1f29b3f7595b9316018
Sukhraj Klair <sukhrajklair@gmail.com>
2019-09-23 13:00:08 -0700

    app working properly. need some refinement in the code

--- src/index.js
+++ src/index.js
@@ -1,10 +1,162 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
 import * as serviceWorker from './serviceWorker';
+import './App.css';
+import {createStore, combineReducers} from 'redux';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+const todo = (state, action) => {
+  switch(action.type){
+    case 'ADD_TODO':
+     return {
+       id: action.id,
+       text: action.text,
+       completed: false
+     };
+    case 'TOGGLE_TODO':
+      if (state.id !== action.id){
+        return state;
+      }
+      return {
+        ...state,
+        completed: !state.completed
+      };
+    default:
+      return state;
+  }
+}
+
+const todos=(state=[], action) => {
+  switch(action.type){
+    case 'ADD_TODO':
+      return [...state, todo({},action)];
+    case 'TOGGLE_TODO':
+      return state.map(t=> todo(t,action));
+    default:
+      return state;
+  }
+}
+
+const visibilityFilter = (state='SHOW_ALL', action) => {
+  switch(action.type){
+    case 'SET_VISIBILITY_FILTER':
+      return action.filter;
+    default:
+      return state;
+  }
+}
+
+//combine all of the reducers into one reducer
+const todoAppReducer = combineReducers({todos,visibilityFilter})
+//create react store
+const todoAppStore = createStore(todoAppReducer);
+
+//create a new Filter link Component
+const FilterLink = ({
+  filter,
+  currentFilter,
+  children,
+}) => {
+  if (filter===currentFilter){
+    return(
+      <span>{children}</span>
+    );
+  }
+  return (
+    <a href='#'
+      onClick={e=> {
+        e.preventDefault();
+        todoAppStore.dispatch({
+          type:'SET_VISIBILITY_FILTER',
+          filter
+        });
+      }}
+    >
+      {children}
+    </a>
+  );
+};
+
+const getVisibleTodos = (
+  todos,
+  filter
+) => {
+  switch (filter) {
+    case 'SHOW_ALL':
+      return todos;
+    case 'SHOW_COMPLETED':
+      return todos.filter(t=>t.completed);
+    case 'SHOW_ACTIVE':
+      return todos.filter(t=>!t.completed);
+    default:
+      return todos
+  }
+}
+//create React component to render the list
+let nextTodoId=0;
+class TodoApp extends React.Component{
+  render(){
+    const {
+      todos,
+      visibilityFilter
+    } = this.props;
+    const visibleTodos = getVisibleTodos(todos,visibilityFilter);
+    return(
+      <div>
+        <input ref={node=>{this.input=node}} />
+        <button onClick = {()=>{
+          todoAppStore.dispatch({
+            type: 'ADD_TODO',
+            text: this.input.value,
+            id: nextTodoId++
+          });
+          this.input.value='';
+        }}>Add Todo</button>
+        <ul>
+          {visibleTodos.map(t=>
+            <li key={t.id}
+              onClick= {()=>{
+                todoAppStore.dispatch({
+                  type:'TOGGLE_TODO',
+                  id: t.id
+                })
+              }}
+              style={{
+                textDecoration:
+                  t.completed?
+                    'line-through' :
+                    'none'
+              }}>
+              {t.text}
+              </li>
+          )}
+        </ul>
+        <p>
+          Show:
+          {' '}
+          <FilterLink filter='SHOW_ALL' currentFilter={visibilityFilter}>All</FilterLink>
+          {' '}
+          <FilterLink filter='SHOW_ACTIVE' currentFilter={visibilityFilter}>Active</FilterLink>
+          {' '}
+          <FilterLink filter='SHOW_COMPLETED' currentFilter={visibilityFilter}>Complteted</FilterLink>
+        </p>
+      </div>
+    );
+  }
+}
+
+const render=()=>{
+  ReactDOM.render(
+    //<TodoApp todos={todoAppStore.getState().todos} filter={todoAppStore.getState().visibilityFilter} />,
+    <TodoApp
+      {...todoAppStore.getState()}
+    />,
+    document.getElementById('root')
+  )
+}
+
+todoAppStore.subscribe(render);
+render();
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
