0f71d3b6358faad78b1517f44bd39d99cd8d7dcf
Sukhraj Klair <sukhrajklair@gmail.com>
2019-09-24 08:29:37 -0700

    Passed the store down using provide() from react-redux, created container components using connect() from react-redux and extracted out action creaters as functions

--- src/index.js
+++ src/index.js
@@ -4,6 +4,7 @@ import './index.css';
 import * as serviceWorker from './serviceWorker';
 import './App.css';
 import {createStore, combineReducers} from 'redux';
+import {Provider, connect} from 'react-redux';
 
 const todo = (state, action) => {
   switch(action.type){
@@ -51,6 +52,31 @@ const todoAppReducer = combineReducers({todos,visibilityFilter})
 //create react store
 //const todoAppStore = createStore(todoAppReducer);
 
+//action creators : mostly for documenting the code and better organization
+let nextTodoId=0;
+const addTodo = (text) => {
+  return{
+    type: 'ADD_TODO',
+    id: nextTodoId++,
+    text
+  };
+};
+
+const setVisibilityFilter = (filter) => {
+  return{
+    type: 'SET_VISIBILITY_FILTER',
+    filter
+  };
+};
+
+const toggleTodo = (id) => {
+  return{
+    type: 'TOGGLE_TODO',
+    id
+  }
+}
+
+//React Components
 const Todo =({
   onClick,
   completed,
@@ -83,66 +109,50 @@ const TodoList = ({
   </ul>
 )
 
-//Created this new container component to contain TodoList presentational component
-class VisibleTodoList extends React.Component{
-  //In this app we re-render the whole application inside the render() function which is subscribed to the createStore
-  //However, it's not ideal to render the whole app on a minor change in the state
-  //Therefore, it makes more sense to move the subscription into individual components using lifecycle method
-  componentDidMount(){
-    //function to unsubscribe is returned when we call the subscribe method
-    //assigning the return to this.unsubscribe field will allow us to invoke it later inside another lifecycle method
-    const{store} = this.context;
-    this.unsubscribe = store.subscribe(()=>
-      this.forceUpdate())
-  }
-  //It's also important to unsubscribe inside the componentWillUnmount
-  componentWillUnmount(){
-    this.unsubscribe();
-  }
-  render(){
-    const{store} = this.context;
-    const props=this.props;
-    const state=store.getState();
+const mapStateToTodoListProps = (state) => {
+  return{
+    todos: getVisibleTodos(state.todos,state.visibilityFilter)
+  };
+};
 
-    return(
-      <TodoList
-        todos={getVisibleTodos(state.todos,state.visibilityFilter)}
-        onTodoClick={todoId=>
-          store.dispatch({
-            type: 'TOGGLE_TODO',
-            id: todoId})
-        }
-      />
-    )
+const mapDispatchToTodoListProps = (dispatch)=>{
+  return{
+    onTodoClick:todoId=>{
+      dispatch(toggleTodo(todoId));
+    }
   }
 }
-//Following code is necessary to receive the context
-VisibleTodoList.contextTypes = {
-  store: React.PropTypes.object
-}
+//The connect function results in a container component that renders the presentational component passed to it
+//It will calculate the props to be passed to the presentational component by merging the object returned from
+//the mapStateToProps() and mapDispatchToProps() functions
+const VisibleTodoList = connect(
+  mapStateToTodoListProps,
+  mapDispatchToTodoListProps
+)(TodoList);
+//Created this new container component to contain TodoList presentational component
+//Using the connect function from react-redux, the presentational container doesn't require the explicitly defined container component anymore
+// class VisibleTodoList extends React.Component{
+//    ......
+// }
 
 //Changed this back to a container component containing its own dispatch callback
-let nextTodoId=0;
-const AddTodo = (props,{store}) => {
+let AddTodo = ({dispatch}) => {
   let input;
 
   return(
     <div>
       <input ref={node=>{input=node}} />
       <button onClick = {()=>{
-        store.dispatch({
-          type:'ADD_TODO',
-          id: nextTodoId++,
-          text:input.value
-        })
+        dispatch(addTodo(input.value));
         input.value='';
       }}>Add Todo</button>
     </div>
   )
 }
-AddTodo.contextTypes = {
-  store: React.ProtoTypes.object
-}
+//We do not need to pass mapStateToProps() becasue we do not access state in AddTodo. Hence this component will not subscribe to the store
+//When nothing is passed to the connect function, it, by default, passes the dispatch function to the component
+AddTodo=connect()(AddTodo)
+
 //Link is just a presentational component
 const Link = ({
   active,
@@ -166,46 +176,33 @@ const Link = ({
   );
 };
 //Split up FilterLink into two components: FilterLink as Container Component and Link as presentational component
-class FilterLink extends React.Component{
-  //In this app we re-render the whole application inside the render() function which is subscribed to the createStore
-  //However, it's not ideal to render the whole app on a minor change in the state
-  //Therefore, it makes more sense to move the subscription into individual components using lifecycle method
-  componentDidMount(){
-    //function to unsubscribe is returned when we call the subscribe method
-    //assigning the return to this.unsubscribe field will allow us to invoke it later inside another lifecycle method
-    const {store}=this.context;
-    this.unsubscribe = store.subscribe(()=>
-      this.forceUpdate())
-  }
-  //It's also important to unsubscribe inside the componentWillUnmount
-  componentWillUnmount(){
-    this.unsubscribe();
-  }
-  render(){
-    const props= this.props;
-    const state = store.getState();
-    const {store}=this.context;
-    return(
-      <Link
-        active = {
-          props.filter ===
-          state.visibilityFilter
-        }
-        onClick = {()=>{
-          store.dispatch({
-            type: 'SET_VISIBILITY_FILTER',
-            filter: props.filter
-          })
-        }}
-      >
-        {props.children}
-      </Link>
-    )
-  }
+//Passing the context to the Link component using connect()
+const mapStateToLinkProps = (state, ownProps)=>{
+  return{
+    active: ownProps.filter === state.visibilityFilter
+  };
+};
+
+const mapDispatchToLinkProps = (dispatch, ownProps)=>{
+  return {
+    onClick: ()=>{
+              dispatch(setVisibilityFilter(ownProps.filter))
+            }
+          }
 }
-FilterLink.contextTypes={
-  store: React.ProtoTypes.object
-}
+
+const FilterLink = connect(
+  mapStateToLinkProps,
+  mapDispatchToLinkProps
+)(Link);
+//The following explicitly declared container isn't required anymore as we are using
+//connect() to provide the state and store to the Link component
+// class FilterLink extends React.Component{
+// ...
+// }
+// FilterLink.contextTypes={
+//   store: React.PropTypes.object
+// };
 //Footer has been converted to a presentational component as well,
 //It doesn't pass the state info to FilterLink anymore
 //FilterLink gets the state itself
@@ -250,20 +247,14 @@ const TodoApp = ()=>(
 
 //The provider component will take the store as its props and make it available to
 //its child and grandchild components using the getChildContext method availabe with React
-class Provider extends React.Component{
-  getChildContext(){
-    return{
-      store:this.props.store
-    };
-  }
-  render(){
-    return this.props.children;
-  }
-}
+//Provider can be imported from the react-redux library instead of declaring it in your code
+// class Provider extends React.Component{
+// ....
+// }
 //The following code is necessary for the context to work
-Provider.childContextTypes = {
-  store: React.ProtoTypes.object
-}
+// Provider.childContextTypes = {
+//   store: React.PropTypes.object
+// };
 
 //Only need to render the app once
 //The container components inside the app are indvidually subscribed to the store and render automatically whenever there is a change in the store
